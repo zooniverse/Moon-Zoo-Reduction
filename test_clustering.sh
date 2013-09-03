de400e48ff1e4ca49514beca02f073982fab1a95
Steven Bamford <steven@stevenbamford.com>
2013-09-03 21:33:59 +0100

    Some more test clustering runs

--- test_clustering.sh
+++ test_clustering.sh
@@ -1,18 +1,49 @@
 cat equal_user_weights.sql | mysql -uroot moonzoo
 
-cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_2_uw clusters/{}.csv markings/{}.csv \
-    1.0 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_2_uw.py.out
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_a_2_uw clusters/{}.csv markings/{}.csv \
+    1.0 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_a_2_uw.py.out
 
-cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_3_uw clusters/{}.csv markings/{}.csv \
-    1.0 3 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_3_uw.py.out
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_a_3_uw clusters/{}.csv markings/{}.csv \
+    1.0 3 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_a_3_uw.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_b_2_uw clusters/{}.csv markings/{}.csv \
+    1.5 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_b_2_uw.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_b_3_uw clusters/{}.csv markings/{}.csv \
+    1.5 3 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_b_3_uw.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_c_2_uw clusters/{}.csv markings/{}.csv \
+    0.75 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_c_2_uw.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_c_3_uw clusters/{}.csv markings/{}.csv \
+    0.75 3 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_c_3_uw.py.out
+
 
 cat create_user_weights.sql | mysql -uroot moonzoo
 
-cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_2_w clusters/{}.csv markings/{}.csv \
-    1.0 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_2_w.py.out
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_a_2_w clusters/{}.csv markings/{}.csv \
+    1.0 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_a_2_w.py.out
 
-cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_4_w clusters/{}.csv markings/{}.csv \
-    1.0 4 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_4_w.py.out
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_a_4_w clusters/{}.csv markings/{}.csv \
+    1.0 4 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_a_4_w.py.out
 
-cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_8_w clusters/{}.csv markings/{}.csv \
-    1.0 8 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_8_w.py.out
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_a_8_w clusters/{}.csv markings/{}.csv \
+    1.0 8 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_a_8_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_b_2_w clusters/{}.csv markings/{}.csv \
+    1.5 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_b_2_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_b_4_w clusters/{}.csv markings/{}.csv \
+    1.5 4 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_b_4_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_b_8_w clusters/{}.csv markings/{}.csv \
+    1.5 8 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_b_8_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_c_2_w clusters/{}.csv markings/{}.csv \
+    0.75 2 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_c_2_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_c_4_w clusters/{}.csv markings/{}.csv \
+    0.75 4 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_c_4_w.py.out
+
+cat selected_nacs | xargs -I{} python mz_cluster.py clusters/{}_A17_c_8_w clusters/{}.csv markings/{}.csv \
+    0.75 8 10 3 4.0 0.4 0.05 30.655 30.800 20.125 20.255 &> mz_cluster_A17_c_8_w.py.out
