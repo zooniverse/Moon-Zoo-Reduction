061bdf5d701d20419fe5bfc4eeb8cdec5edd2b07
Steven Bamford <steven@stevenbamford.com>
2014-01-15 11:02:07 +0000

    Examples of running with recent changes

---
+++ test_clustering_new.sh
@@ -0,0 +1,11 @@
+python ../../scripts/mz_cluster.py -f clusters/715_a_2_uw markings/715.csv New_CC/truth.csv New_CC/ROI_715.png 1.0 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 715_a_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/715_b_2_uw markings/715.csv New_CC/truth.csv New_CC/ROI_715.png 1.5 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 715_b_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/715_c_2_uw markings/715.csv New_CC/truth.csv New_CC/ROI_715.png 0.75 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 715_c_2_uw.out &
+
+python ../../scripts/mz_cluster.py -f clusters/648_a_2_uw markings/648.csv New_CC/truth.csv New_CC/ROI_648.png 1.0 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 648_a_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/648_b_2_uw markings/648.csv New_CC/truth.csv New_CC/ROI_648.png 1.5 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 648_b_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/648_c_2_uw markings/648.csv New_CC/truth.csv New_CC/ROI_648.png 0.75 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> 648_c_2_uw.out &
+
+python ../../scripts/mz_cluster.py -f clusters/all_a_2_uw markings/all.csv New_CC/truth.csv New_CC/ROI_715.png 1.0 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> all_a_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/all_b_2_uw markings/all.csv New_CC/truth.csv New_CC/ROI_715.png 1.5 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> all_b_2_uw.out &
+python ../../scripts/mz_cluster.py -f clusters/all_c_2_uw markings/all.csv New_CC/truth.csv New_CC/ROI_715.png 0.75 2 10 3 4.0 0.4 100 30.699 30.880 20.200 20.275 &> all_c_2_uw.out &
